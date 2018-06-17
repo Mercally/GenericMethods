c7692210285465310ed6f8cbd1e115fbeb09ef64
Mercally <josuemercally@gmail.com>
2018-06-17 01:09:52 -0600

    Cambios de nuggets

---
+++ CGC_GenericMethods-BackEnd/CGC_GM_BE.Services/CGC_GM_BE.Services.ServiceCatalogoApi/Controllers/CatalogosController.cs
@@ -0,0 +1,34 @@
+﻿using CGC_GM_BE.Business;
+using CGC_GM_BE.Common.Entities;
+using CGC_GM_BE.Services.Metadata.ServiceCatalogoApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CGC_GM_BE.Services.ServiceCatalogoApi.Controllers
+{
+    [RoutePrefix("api/Catalogos")]
+    public class CatalogosController : ApiController, ICatalogosControllerApi
+    {
+
+        Catalogo_BL Catalogo = new Catalogo_BL();
+
+        [HttpGet]
+        [Route("Filtro/{Tabla}/{Campo}")]
+        public List<Catalogo> ObtenerCatalogo(string Tabla, string Campo)
+        {
+            return Catalogo.Consulta(Tabla, Campo);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Catalogo ObtenerCatalogoPorId(int id)
+        {
+            return Catalogo.ConsultaPorId(id);
+        }
+    }
+
+}
